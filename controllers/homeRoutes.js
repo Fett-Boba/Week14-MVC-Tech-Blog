6f3f0795b0cd76d7ec7776a79dd4461e2fe09c46
Boba Fett <thugfencer@gmail.com>
2021-08-08 00:52:11 -0700

    Get posts to display on mainpage with no auth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,39 +1,59 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+// Get all posts for all users. No login required.
+router.get('/', async (req, res) => {
      try {
-          const userData = await User.findAll({
-               attributes: { exclude: ['password'] },
-               order: [['username', 'ASC']],
+
+          // const userData = await User.findAll({
+          //      attributes: { exclude: ['password'] },
+          //      order: [['username', 'ASC']],
+          // });
+
+          const postData = await Post.findAll({
+               include: [{ model: User, attributes: ['id', 'username'] }],
           });
 
-          const users = userData.map((project) => project.get({ plain: true }));
+          // const users = userData.map((project) => project.get({ plain: true }));
+          const posts = postData.map((post) => post.get({ plain: true }));
 
-          res.render('homepage', {
-               users,
-               // Pass the logged in flag to the template
-               logged_in: req.session.logged_in,
-          });
+          // res.render('homepage', { users, logged_in: req.session.logged_in, });
+          res.render('homepage', { posts, logged_in: req.session.logged_in, });
+
      } catch (err) {
+          console.log(err);
           res.status(500).json(err);
      }
 });
 
+//const router = require('express').Router();
+//const { Post, User } = require('../models');
+// Get all posts, all users. No login required
+// router.get('/', async (req, res) => {
+//      try {
+//           const postData = await Post.findAll({
+//                include: [{ model: User, attributes: ['id', 'username'] }],
+//           });
+//           const posts = postData.map((post) => post.get({ plain: true }));
+//           res.render('homepage', { posts, loggedIn: req.session.loggedIn, });
+//      } catch (err) {
+//           console.log(err);
+//           res.status(500).json(err);
+//      }
+// });
+
+// Render login Route
 router.get('/login', (req, res) => {
-     // If a session exists, redirect the request to the homepage
      if (req.session.logged_in) {
           res.redirect('/');
           return;
      }
-
      res.render('login');
 });
 
+// Render signup route
 router.get('/signup', (req, res) => {
-     // If a session exists, redirect the request to the homepage
      if (req.session.logged_in) {
           res.redirect('/');
           return;
@@ -41,7 +61,7 @@ router.get('/signup', (req, res) => {
      res.render('signup');
 });
 
-
-
-
 module.exports = router;
+
+
+
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,7 @@
-// stub for now
+module.exports = {
+     // Helper to reformat the date
+     format_date: (date) => {
+          return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()
+               }`;
+     },
+};
